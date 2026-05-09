7ede9c2db2d35d0020da0d3a7619ebdbe36c149b
genmodmaster <94315046+genmodmaster@users.noreply.github.com>
2026-05-09 21:00:30 +0200

    Update next.config.mjs for output and basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default nextConfig
+
+/** @type {import('next').NextConfig} */
+const repo = 'v0-fortune-teller-website'
+
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   images: {
     unoptimized: true,
   },
